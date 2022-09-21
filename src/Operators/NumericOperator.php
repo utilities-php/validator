ea315f7d7cea3b63da9dc0acdbb2148d2a710a1d
shahradelahi <shahradq@gmail.com>
2022-09-21 07:34:59 +0000

    Introducing the `Validator` package and renaming the `Router` package.

--- src/Operators/NumericOperator.php
+++ src/Operators/NumericOperator.php
@@ -18,116 +18,42 @@ namespace Utilities\Validator\Operators;
 trait NumericOperator
 {
 
-    /**
-     * Is greater than.
-     *
-     * @error_message This value is not greater than {[0]}.
-     * @error_code NOT_GREATER_THAN
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isGreaterThan(int $limit): bool
+    protected function isGreaterThan(int $limit): bool
     {
         return $this->data > $limit;
     }
 
-    /**
-     * Is less than.
-     *
-     * @error_message This value is not less than {[0]}.
-     * @error_code NOT_LESS_THAN
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isLessThan(int $limit): bool
+    protected function isLessThan(int $limit): bool
     {
         return $this->data < $limit;
     }
 
-    /**
-     * Is greater than or equal.
-     *
-     * @error_message This value is not greater than or equal to {[0]}.
-     * @error_code NOT_GREATER_THAN_OR_EQUAL
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isGreaterThanOrEqual(int $limit): bool
+    protected function isGreaterThanOrEqual(int $limit): bool
     {
         return $this->data >= $limit;
     }
 
-    /**
-     * Is less than or equal.
-     *
-     * @error_message This value is not less than or equal to {[0]}.
-     * @error_code NOT_LESS_THAN_OR_EQUAL
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isLessThanOrEqual(int $limit): bool
+    protected function isLessThanOrEqual(int $limit): bool
     {
         return $this->data <= $limit;
     }
 
-    /**
-     * Is equal.
-     *
-     * @error_message This value is not equal to {[0]}.
-     * @error_code NOT_EQUAL
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isEqual(int $limit): bool
+    protected function isEqual(int $limit): bool
     {
         return $this->data == $limit;
     }
 
-    /**
-     * Is not equal.
-     *
-     * @error_message This value is equal to {[0]}.
-     * @error_code EQUAL
-     *
-     * @param int $limit
-     * @return bool
-     */
-    public function isNotEqual(int $limit): bool
+    protected function isNotEqual(int $limit): bool
     {
         return $this->data != $limit;
     }
 
-    /**
-     * Is between.
-     *
-     * @error_message This value is not between {[0]} and {[1]}.
-     * @error_code NOT_BETWEEN
-     *
-     * @param int $min
-     * @param int $max
-     * @return bool
-     */
-    public function isBetween(int $min, int $max): bool
+    protected function isBetween(int $min, int $max): bool
     {
         return $this->data >= $min && $this->data <= $max;
     }
 
-    /**
-     * Is not between.
-     *
-     * @error_message This value is between {[0]} and {[1]}.
-     * @error_code BETWEEN
-     *
-     * @param int $min
-     * @param int $max
-     * @return bool
-     */
-    public function isNotBetween(int $min, int $max): bool
+    protected function isNotBetween(int $min, int $max): bool
     {
         return $this->data < $min || $this->data > $max;
     }
--- tests/Constraints/NumberConstraintTest.php
+++ tests/Constraints/NumberConstraintTest.php
@@ -6,10 +6,4 @@ namespace UtilitiesTests\Validator\Constraints;
 class NumberConstraintTest extends \PHPUnit\Framework\TestCase
 {
 
-    public function testNumberConstraint()
-    {
-        $validator = \Utilities\Validator\Validate::number(10);
-        $this->assertTrue($validator->isNumber());
-    }
-
 }
